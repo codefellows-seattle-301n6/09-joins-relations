ebc46b170b5596cbbbc7d4564eb41b796f7aef2e
Dana Kulp <dkulp23@gmail.com>
2016-12-08 19:50:05 -0800

    Started script for city filter function.

--- zip-codes/scripts/search.js
+++ zip-codes/scripts/search.js
@@ -6,7 +6,6 @@
     webDB.execute(
       'SELECT state FROM zips',
       function(states){
-        console.log(states);
         states.forEach(function(ele) {
           if ($('#state-select option[value="' + ele.state + '"]').length === 0) {
             $('#state-select').append('<option value="' + ele.state + '">' + ele.state + '</option>');
@@ -15,8 +14,25 @@
       }
     );
   };
+
+  var cityFilter = function() {
+    $('#state-select').on('change', function(state){
+      console.log(state.val());
+      webDB.execute(
+        'SELECT city FROM zips WHERE state=' + state,
+        function(cities){
+          cities.forEach(function(ele) {
+            if ($('#city-select option[value="' + ele.city + '"]').length === 0) {
+              $('#city-select').append('<option value="' + ele.city + '">' + ele.city + '</option>');
+            }
+          });
+        }
+      );
+    });
+  };
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
 
   stateFilter();
+  cityFilter();
 })(window);
