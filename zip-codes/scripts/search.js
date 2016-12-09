83ab68e3c01ca1a66ff6d8567527e986cbdfa23f
Dana Kulp <dkulp23@gmail.com>
2016-12-08 19:10:36 -0800

    Started working on script to populate the state search option menu.

--- zip-codes/scripts/search.js
+++ zip-codes/scripts/search.js
@@ -2,8 +2,17 @@
 
 (function(module) {
 
+  var stateFilter = function() {
+    webDB.execute(
+      'SELECT state FROM zips',
+      function(state){
+        console.log(state.state);
+        // $('#state-select').append('<option value="' + state + '">' + state + '</option>');
+      }
+    );
+  };
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
 
-
-})(window)
+  stateFilter();
+})(window);
