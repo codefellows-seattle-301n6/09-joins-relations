3f2cd7da7fbaba6c3bc9fd1527c6ee55a424491f
Dana Kulp <dkulp23@gmail.com>
2016-12-10 10:45:25 -0800

    Started script for infowindow.

--- zip-codes/scripts/map.js
+++ zip-codes/scripts/map.js
@@ -13,7 +13,9 @@ function initMap(location) {
     position: location,
     map: map
   });
+  // var infoWindow = new google.maps.infoWindow(
 
+  // )
   // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
 
 }
